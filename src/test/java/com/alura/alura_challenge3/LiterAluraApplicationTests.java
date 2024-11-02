4b2184df6d9e9421398de15dc139eedd14ebad3e
Jose Ortega <jmortegafreire@gmail.com>
2024-11-02 17:31:29 -0300

    Seting up enviroment

---
+++ src/test/java/com/alura/alura_challenge3/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.alura_challenge3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
